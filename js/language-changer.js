368e43e51e332c0074b5d4c9cb12dabe357d6d69
Huaraz2 <adam.pohl9999@gmail.com>
2016-03-22 16:32:40 +0000

    Hopefully this fixes things

--- js/language-changer.js
+++ js/language-changer.js
@@ -11,37 +11,34 @@ $(document).ready(function () {
     //             - Updating the dropdown selection
     //             - If the facility is available, saving the user preference
     var changeLanguage = function(short) {
+        // Hide all language details
+        $(langnames).hide();
 
-	      // Hide all language details
-	      $(langnames).hide();
+        // Unhide the relevant language
+        $(langnames + "#" + short).show();
 
-	      // Unhide the relevant language
-	      $(langnames + "#" + short).show();
+        // Update the dropdown
+        $(langchooser).val(short);
 
-	      // Update the dropdown
-	      $(langchooser).val(short);
-
-	      // If local storage is available, save the language so the change
-	      // is persistent both accross page loads and entire sessions
-	      if (typeof(Storage) !== undefined) {
-	          localStorage.setItem("lang", short);
-	      }
+        // If local storage is available, save the language so the change
+        // is persistent both accross page loads and entire sessions
+        if (typeof(Storage) !== undefined) {
+            localStorage.setItem("lang", short);
+        }
     };
 
     // In case there already is a saved preference, load that theme
     // otherwise load the default
     if (localStorage.lang) {
-	      changeLanguage(localStorage.lang);
+        changeLanguage(localStorage.lang);
     } else {
-	      changeLanguage("en");
+	    changeLanguage("en");
     }
 
     // Finally this code is only run when the user makes a choice using the
     // dropdown in the footer
     $(langchooser).change(function() {
-
-	      var new_lang = $(langchooser).val();
-	      changeLanguage(new_lang);
-
+        var new_lang = $(langchooser).val();
+	    changeLanguage(new_lang);
     });
 });
