8f57ba14136b6394ae3ae78f1eb25316418d7d1e
Huaraz2 <adam.pohl9999@gmail.com>
2016-03-22 21:15:53 +0000

    Languages are DONE :imp: :dragon:

--- js/language-changer.js
+++ js/language-changer.js
@@ -6,16 +6,11 @@ $(document).ready(function () {
     var langnames = "div.language";
     var langchooser = "select#lang-chooser";
 
-    // This function handles everything to do with changing the theme
+    // This function handles everything to do with changing the language
     // including:
     //             - Updating the dropdown selection
     //             - If the facility is available, saving the user preference
     var changeLanguage = function(short) {
-        // Hide all language details
-        $(langnames).hide();
-
-        // Unhide the relevant language
-        $(langnames + "#" + short).show();
 
         // Update the dropdown
         $(langchooser).val(short);
@@ -24,7 +19,7 @@ $(document).ready(function () {
         // is persistent both accross page loads and entire sessions
         if (typeof(Storage) !== undefined) {
             localStorage.setItem("lang", short);
-        }
+        };
     };
 
     // In case there already is a saved preference, load that theme
@@ -33,12 +28,17 @@ $(document).ready(function () {
         changeLanguage(localStorage.lang);
     } else {
 	    changeLanguage("en");
-    }
+    };
 
     // Finally this code is only run when the user makes a choice using the
     // dropdown in the footer
     $(langchooser).change(function() {
         var new_lang = $(langchooser).val();
 	    changeLanguage(new_lang);
+        var s = "http://"
+        var x = location.host;
+        var y = "/";
+        var z = s.concat(x, y, new_lang);
+        window.open(z, "_self");
     });
 });
